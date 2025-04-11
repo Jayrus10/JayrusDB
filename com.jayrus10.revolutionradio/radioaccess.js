6266dace6fbababb1064891478e1250bfe7a28d9
Jayro González <156376653+Jayrus10@users.noreply.github.com>
2025-04-11 15:26:46 -0400

    Add files via upload

---
+++ com.jayrus10.revolutionradio/radioaccess.js
@@ -0,0 +1,19 @@
+// Función para obtener los datos del archivo JSON
+async function obtenerDatosJSON() {
+    try {
+        const respuesta = await fetch('com.jayrus10.revolutionradio/server_state.json'); // Reemplaza 'datos.json' con la ruta real a tu archivo JSON
+        const datos = await respuesta.json();
+        
+        // Actualiza el enlace de descarga en la página
+        document.getElementById('enlace-descarga').href = datos.url_descarga;
+        
+        // Muestra la versión disponible
+        document.getElementById('version-numero').innerText = `Versión ${datos.version_disponible}`;
+        
+    } catch (error) {
+        console.error('Error al cargar el archivo JSON:', error);
+    }
+}
+
+// Llama a la función al cargar la página
+document.addEventListener('DOMContentLoaded', obtenerDatosJSON);
