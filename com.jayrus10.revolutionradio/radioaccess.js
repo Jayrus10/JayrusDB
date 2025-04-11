cd9670aac4e74c2520cf7552f3792e79fac67503
Jayro González <156376653+Jayrus10@users.noreply.github.com>
2025-04-11 15:44:39 -0400

    Update radioaccess.js

--- com.jayrus10.revolutionradio/radioaccess.js
+++ com.jayrus10.revolutionradio/radioaccess.js
@@ -8,7 +8,7 @@ async function obtenerDatosJSON() {
         document.getElementById('enlace-descarga').href = datos.url_descarga;
         
         // Muestra la versión disponible
-        document.getElementById('version-numero').innerText = `Versi&oacute;n: ${datos.version_disponible}`;
+        document.getElementById('version-numero').innerText = `Version: ${datos.version_disponible}`;
         
     } catch (error) {
         console.error('Error al cargar el archivo JSON:', error);
